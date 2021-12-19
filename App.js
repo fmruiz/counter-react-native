582ba2e8ee7f176bcf71db806b0c91f6a48da240
franco <x.francoruiz@gmail.com>
2021-12-19 17:21:36 -0300

    initial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hola mundo</Text>
+    </View>
+  )
+}
+
+export default App;
+
